688d1e8970df3fcd28ac66c07977aad59c66d8b4
Corey Ferguson <corey.t.ferguson@gmail.com>
2017-05-08 17:48:36 -0700

    change-set

---
+++ change-set.js
@@ -0,0 +1,21 @@
+
+const fs = require('fs');
+const AWS = require('aws-sdk');
+
+AWS.config.update({
+  region: 'us-west-2'
+});
+
+const cloudFormationTemplate = fs.readFileSync('./cloudformation-template.yml').toString();
+console.log('cloudFormationTemplate:', cloudFormationTemplate);
+
+const cloudformation = new AWS.CloudFormation();
+
+cloudformation.createChangeSet({
+  ChangeSetName: 'test-cloudformation-changeset',
+  StackName: 'test-cloudformation',
+  TemplateBody: cloudFormationTemplate
+}, function(err, data) {
+  if (err) console.log(err, err.stack);
+  else     console.log(data);
+});
